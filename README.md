7cf3f36b499213f8717eff21118090345e642637
anu061 <anu061@gmail.com>
2016-05-05 16:47:08 -0700

    Uploading README, R Script file, CodeBook and 'tidyData.txt'

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# GettingAndCleaningData
+###
+# GettingAndCleaningData Project - README
+#
+# Author: AK
+#
+###
+
+This is a README file for the human activity recognition project, which is a part of the "Getting and Cleaning Data" course. 
+
+The purpose of this project is to demonstrate ways to collect, work with, and clean a data set. 
+The goal is to prepare tidy data that can be used for later analysis. 
+
+
+# Data Set
+Data for the project is downloaded from the following link:
+https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+
+A full description about the data is available at the below site (where the data was originally obtained from):
+http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
+
+- The data has been recorded through experiments carried out with a group of 30 volunteers within an age bracket of 19-48 years. 
+- Each person performed six activities (WALKING, WALKING_UPSTAIRS, WALKING_DOWNSTAIRS, SITTING, STANDING, LAYING) wearing a smartphone (Samsung Galaxy S II) on the waist. 
+- Using its embedded accelerometer and gyroscope, 3-axial linear acceleration (total acceleration and estimated body acceleration) and 3-axial angular velocity were captured at a constant rate of 50Hz. 
+- The experiments were video-recorded to label the data manually. 
+- The obtained dataset has been randomly partitioned into two sets, where 70% of the volunteers was selected for generating the training data and 30% the test data.
+
+The "data/train" folder in the downloaded dataset contains the training data, and the "data/test" folder contains all the test data.
+
+The specific files in the downloaded dataset are:
+
+- 'data/features.txt': List of all features.
+- 'data.activity_labels.txt': Links the class labels with their activity name.
+- "data/train/X_train.txt" & "data/test/X_test.txt": A 561-feature vector with time and frequency domain variables, corresponding to the measurements from the accelermoter & gyroscope.
+- "data/train/y_train.txt" & "data/test/y_test.txt": Activity identifiers corresponding to the recorded activity.
+- "data/train/subject_train.txt" & "data/test/subject_test.txt": Identifier for subjects who carried out the experiments.
+
+
+# run_analysis.R 
+
+The "run_analysis.R" script included in the repository performs the following overall steps:
+
+1. Merges the training and the test sets to create one data set.
+2. Extracts only the measurements on the mean and standard deviation for each measurement.
+3. Uses descriptive activity names to name the activities in the data set
+4. Appropriately labels the data set with descriptive variable names.
+5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
---
+++ run-analysis.R
@@ -0,0 +1,91 @@
+######
+# Getting and Cleaning Data Course Project
+# 
+# Author: AK
+# 
+# The data linked to from the course website represent data collected from the 
+# accelerometers from the Samsung Galaxy S smartphone. 
+# 
+# A full description is available at the site where the data was obtained:
+# http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
+#       
+# Here are the data for the project:
+#       https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+#       
+# The following code (run_analysis.R) does the following:
+# - Merges the training and the test sets to create one data set.
+# - Extracts only the measurements on the mean and standard deviation for each 
+#       measurement.
+# - Uses descriptive activity names to name the activities in the data set 
+# - Appropriately labels the data set with descriptive variable names.
+# - From the data set in step 4, creates a second, independent tidy data set 
+#       with the average of each variable for each activity and each subject.
+#
+################################################################################
+
+
+# read in training & test data.. 
+# ..subject data from subject_train.txt
+# ..feature measurements (via accelerometer & gyroscope) from x_train.txt 
+# ..activity data from y_train.txt
+
+subTrain <- read.table("data/train/subject_train.txt")
+xTrain <- read.table("data/train/X_train.txt")
+yTrain <- read.table("data/train/y_train.txt")
+
+subTest <- read.table("data/test/subject_test.txt")
+xTest <- read.table("data/test/X_test.txt")
+yTest <- read.table("data/test/y_test.txt")
+
+
+# identify features corresponding to the mean and standard deviation for each measurement
+featuresAll <- read.table("data/features.txt")
+featuresSelect <- grep(".*mean.*|.*std.*", featuresAll[,2])
+featuresSelectNames <- as.character(featuresAll[featuresSelect,2])
+
+# clean up feature names: remove/replace '-' and '()'to make them more readable and less error-prone
+featuresSelectNames <- gsub('-mean', 'Mean', featuresSelectNames)
+featuresSelectNames <- gsub('-std', 'Std', featuresSelectNames)
+featuresSelectNames <- gsub('[-()]', '', featuresSelectNames)
+
+        
+# select training & test data corresponding to mean and standard deviation features
+xTrainSelect <- xTrain[featuresSelect]
+xTestSelect <- xTest[featuresSelect]
+
+# read in activity labels to name the activities in the data set (yTrain & yTest) 
+actLabels <- read.table("data/activity_labels.txt")
+
+# convert subjects & activities to factor
+subTrain[,1] <- as.factor(subTrain[,1])
+yTrain[,1] <- factor(yTrain[,1], levels = actLabels[,1], labels = actLabels[,2])
+
+subTest[,1] <- as.factor(subTest[,1])
+yTest[,1] <- factor(yTest[,1], levels = actLabels[,1], labels = actLabels[,2])
+
+
+# merge data corresponding to the subjects, activity, features..
+trainData <- cbind(subTrain, yTrain, xTrainSelect)
+testData <- cbind(subTest, yTest, xTestSelect)
+
+# combine training and test data into one big dataset
+allData <- rbind(trainData, testData)
+
+
+# assign meaningful names to the variables in the combined data set
+colnames(allData) <- c("subject", "activity", featuresSelectNames)
+
+
+# From allData, create a tidy data set with the average of each variable for 
+# each activity and each subject.
+
+# tidyDataMelt contains: subject, activity, variable, value
+tidyDataMelt <- melt(allData, id = c("subject", "activity"))
+
+#tidyData contains: subject, activity, feature1, feature2,........,feature79
+tidyData <- dcast(tidyDataMelt, subject + activity ~ variable, mean)
+  
+#write tidyData into a text file
+write.table(tidyData, file = "tidyData.txt", row.names = FALSE, col.names = TRUE)
+
+str(tidyDataMelt)
